9e09a1c1d5da29c2957f05aaecaadba9b5c6bf7d
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2021-08-01 13:45:58 +0300

    fix: babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      'module:metro-react-native-babel-preset',
+      'module:react-native-dotenv',
+      '@babel/preset-flow',
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.ios.ts', '.android.ts'],
+          alias: {
+            components: './src/components',
+          },
+        },
+      ],
+      ['transform-class-properties'],
+    ],
+  };
 };
