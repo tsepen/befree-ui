5d02fc5f0434bf56b194a77b6b18418ede90743f
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2021-08-01 21:04:07 +0300

    fix: babel

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// /**
-//  * @format
-//  */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-//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
-//4d54e5e0-a421-44df-8405-1d94f5a6aba1
 export * from './src/components';
 
 export {default} from './storybook';
